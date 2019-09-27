a23348bcae6853a6ad8f4d91a4062a8967e1793a
Kwasniok <Kwasniok@users.noreply.github.com>
2019-09-27 21:36:24 +0200

    extended core.hand

--- core/hand.py
+++ core/hand.py
@@ -6,9 +6,13 @@ class Hand(Owned):
         Owned.__init__(self, owner=owner)
         self._name = name
         self._cards = []
+        self._limit = limit
 
     def __str__(self):
         return self._name
+
+    def __contains__(self, card):
+        return card in self._cards
 
     def add_card(self, card):
         self._cards.append(card)
@@ -26,3 +30,9 @@ class Hand(Owned):
                     + "`"
                 )
             )
+
+    def above_limit(self):
+        return len(self._cards) > self._limit
+
+    def size(self):
+        return len(self._cards)
