36fabcc7293f2d05e54c0179a9cfc08e9a2be2b6
Kwasniok <Kwasniok@users.noreply.github.com>
2019-09-30 23:23:03 +0200

    added game.player

---
+++ game/player.py
@@ -0,0 +1,16 @@
+from core.owning import Owner
+from core.hand import Hand
+from .realm import Realm
+
+
+class Player(Owner):
+    def __init__(self, name, color):
+        Owner.__init__(self, name=name, color=color)
+        self._hand = Hand(name="hand of " + name, owner=self)
+        self._realm = Realm("relm of" + name, owner=self)
+
+    def get_hand(self):
+        return self._hand
+
+    def get_realm(self):
+        return self._realm
