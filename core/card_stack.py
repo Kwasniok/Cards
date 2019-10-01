22f9a2db658140d62b9e1be2b20336b77dc998ef
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-01 04:06:33 +0200

    overhaul of core.card_stack

--- core/card_stack.py
+++ core/card_stack.py
@@ -3,29 +3,66 @@ from .owning import Owned
 
 class Card_Stack(Owned):
     def __init__(self, name, cards=None):
-        self.name = name
+        self._name = name
         if cards is None:
             self._cards = []
         else:
-            self.cards = cards
+            self._cards = cards
 
     def __str__(self):
-        return self.name
+        return self._name
 
-    def can_pick_top(self):
-        return len(self.cards) > 0
+    def __len__(self):
+        return len(self._cards)
 
-    def pick_top(self):
-        card = self.cards[-1]
-        self.cards.remove(card)
+    def __contains__(self, card):
+        return card in self._cards
+
+    def __iter__(self):
+        return iter(self._cards)
+
+    def can_pop_top(self):
+        return len(self._cards) > 0
+
+    def get_top(self):
+        return self._cards[-1]
+
+    def pop_top(self):
+        card = self._cards[-1]
+        self._cards.remove(card)
         return card
 
-    def get_cards(self):
-        return self.cards
+    def push_top(self, card):
+        if card in self._cards:
+            raise (
+                ValueError(
+                    "Cannot push card `"
+                    + str(card)
+                    + "` on top of card stack `"
+                    + str(self)
+                    + "` because it is allready in this stack."
+                )
+            )
+        else:
+            self._cards.append(card)
 
-    def pick_card(self, card):
-        if card in self.cards:
-            self.cards.remove(card)
+    def push_bottom(self, card):
+        if card in self._cards:
+            raise (
+                ValueError(
+                    "Cannot push card `"
+                    + str(card)
+                    + "` on botttom of card stack `"
+                    + str(self)
+                    + "` because it is allready in this stack."
+                )
+            )
+        else:
+            self._cards.insert(0, card)
+
+    def remove(self, card):
+        if card in self._cards:
+            self._cards.remove(card)
         else:
             raise (
                 ValueError(
