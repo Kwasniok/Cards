0d9965b270e338a85a24de6f862da0a9cf50b73a
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-01 20:42:09 +0200

    added core.util.random_pop

--- core/util.py
+++ core/util.py
@@ -3,3 +3,9 @@ from random import randrange
 
 def random_pick(list):
     return list[randrange(len(list))]
+
+
+def random_pop(list):
+    elem = random_pick(list)
+    list.remove(elem)
+    return elem
