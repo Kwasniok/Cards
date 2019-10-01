d2d716ca8281925b7e7bbdf07998baae008b2879
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-01 03:10:27 +0200

    added primitive game.game prototype

---
+++ game/game.py
@@ -0,0 +1,21 @@
+from core.color import BLACK, WHITE
+from core.owning import Owner
+from core.dice import Dice
+from core.card_stack import Card_Stack
+from .cards import *
+from .events import EVENT_DICE_OUTCOMES
+from .player import Player
+
+
+class Game:
+    def __init__(self):
+        self._number_dice = Dice(name="number dice", outcomes=range(1, 7))
+        self._event_dice = Dice(
+            name="number dice", outcomes=EVENT_DICE_OUTCOMES
+        )
+        self._player1 = Player(name="player 1", color=BLACK)
+        self._player2 = Player(name="player 2", color=WHITE)
+        self._expansion_card_stacks = []
+        for i in range(1, 5 + 1):
+            stack = Card_Stack(name="expansion card stack " + str(i))
+            self._expansion_card_stacks.append(stack)
