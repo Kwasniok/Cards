3afe469a939040ac415c4de2a74e65b30d55e326
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-01 03:33:18 +0200

    grouped tests in game.test.cards

--- game/test/cards.py
+++ game/test/cards.py
@@ -9,11 +9,17 @@ from ..cards import (
 
 
 class Test(unittest.TestCase):
-    def test_general(self):
+    def test_structure(self):
+        cards = [Road_Card(), Settlement_Card(), Town_Card()]
+        self.assertTrue(len(cards) > 2)
+        for c in cards:
+            self.assertTrue(isinstance(str(c), str))
+            self.assertTrue(isinstance(c.text(), str))
+            # card name in card text (might not be a good idea)
+            self.assertTrue(str(c) in c.text())
+
+    def test_resource(self):
         cards = [
-            Road_Card(),
-            Settlement_Card(),
-            Town_Card(),
             Resource_Card(LOGS, 1, 0),
             Resource_Card(BRICKS, 1, 0),
             Resource_Card(GRAIN, 3, 0),
