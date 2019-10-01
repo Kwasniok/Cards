160f4f95af0bfc7ec84ca800e1cd1c4a589fca7c
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-01 03:31:59 +0200

    added game.expansion_cards prototypes

---
+++ game/expansion_cards.py
@@ -0,0 +1,46 @@
+from abc import abstractmethod
+from core.card import Card
+
+
+class Expansion_Card(Card):
+    def __init__(self, name):
+        Card.__init__(self, name)
+
+    @abstractmethod
+    def playable(self, context):
+        pass
+
+
+class Action_Card(Card):
+    def __init__(self, name):
+        Expansion_Card.__init__(self, name)
+
+
+class Building_Card(Card):
+    def __init__(self, name):
+        Expansion_Card.__init__(self, name)
+
+    def mill_points(self, context):
+        return 0
+
+    def knight_points(self, context):
+        return 0
+
+    def win_points(self, context):
+        return 0
+
+    def on_construction(self, context):
+        return
+
+    def on_destruction(self, context):
+        return
+
+
+class Small_Building_Card(Card):
+    def __init__(self, name):
+        Building_Card.__init__(self, name)
+
+
+class Large_Building_Card(Card):
+    def __init__(self, name):
+        Building_Card.__init__(self, name)
