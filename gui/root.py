e79c5a13677c5ee6df8dda4eb8b9a0d8580c257b
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-03 22:54:58 +0200

    added gui.root

---
+++ gui/root.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+
+
+class Root:
+    _root = None
+    _application = None
+
+    @staticmethod
+    def register_application(application):
+        if not (Root._application is None):
+            raise (RuntimeError("Only one Application is allowed."))
+        Root._application = application
+
+    @staticmethod
+    def unregister_application():
+        Root._application = None
+
+    def get_root():
+        if Root._root is None:
+            Root._root = tk.Tk()
+            # disable default window
+            Root._root.withdraw()
+        return Root._root
