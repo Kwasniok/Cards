84a81854ecd97fcce0d7ff3a0d80ae607afbc53a
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-03 22:55:18 +0200

    added gui.window

---
+++ gui/window.py
@@ -0,0 +1,26 @@
+import tkinter as tk
+from tkinter import messagebox
+
+
+class Window:
+    def __init__(self, application, title, width, height, x, y):
+        self._application = application
+        self._toplevel = tk.Toplevel(
+            self._application.get_master(), width=width, height=height
+        )
+        self._toplevel.geometry("%dx%d+%d+%d" % (width, height, x, y))
+        self._toplevel.title(title)
+        self._toplevel.protocol("WM_DELETE_WINDOW", lambda: self.at_quit())
+
+    def destroy(self):
+        self._toplevel.destroy()
+        self._toplevel = None
+
+    def quit(self):
+        self.destroy()
+
+    def at_quit(self):
+        if messagebox.askokcancel(
+            "Quit Window", "Do you want to quit this window?"
+        ):
+            self.quit()
