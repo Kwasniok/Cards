85b146357b2db2c9d26447022aee720712924dea
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-03 19:20:47 +0200

    fixed name in game.test.game_state

--- game/test/game_state.py
+++ game/test/game_state.py
@@ -4,4 +4,4 @@ from ..game_state import Game_State
 
 class Test(unittest.TestCase):
     def test_general(self):
-        g = Game_State(name="test game")
+        g = Game_State(name="test game state")
