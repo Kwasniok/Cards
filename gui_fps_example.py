0a7c9d6a68d32bc85065404c18679668da853986
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-04 17:58:17 +0200

    added example with simple app+fps test

---
+++ gui_fps_example.py
@@ -0,0 +1,43 @@
+import time
+import tkinter as tk
+from gui.application import Application
+from gui.update import Updatable
+from gui.window import Window
+
+
+def on_click():
+    print("clicked")
+
+
+class Test_Updatable(Updatable):
+    def __init__(self):
+        Updatable.__init__(self)
+        self._counter = 0
+        self._t0 = time.time()
+        self._n = 5
+        self._fps = None
+
+    def on_update(self):
+        self._counter += 1
+        if self._counter % self._n == 0:
+            t = time.time()
+            self._fps = float(self._n) / (t - self._t0)
+            self._t0 = t
+        print("frame_nr = " + str(self._counter) + " fps = " + str(self._fps))
+        for i in range(100000):
+            pass
+
+
+def main():
+    application = Application(fps_bound=60)
+    window = Window(application, title="A", x=100, y=100, width=150, height=150)
+    button = tk.Button(window._toplevel, text="text", command=on_click)
+    button.grid()
+    updatable = Test_Updatable()
+    application.get_frame_updater().register(updatable)
+    application.run()
+    application.destroy()
+
+
+if __name__ == "__main__":
+    main()
