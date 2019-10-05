0414a33bed24bb1a0f103920a2ebb2aaac5a3f91
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-05 16:24:01 +0200

    added test.gui_test

---
+++ test/gui_test.py
@@ -0,0 +1,66 @@
+import tkinter as tk
+from gui.application import Application
+from gui.window import Window
+from gui.update import Updatable
+
+
+class Test_Window(Window, Updatable):
+    def __init__(self, application, x, y):
+        width = 512
+        height = 512
+        Window.__init__(
+            self,
+            application=application,
+            title="A Test Window",
+            width=width,
+            height=height,
+            x=x,
+            y=y,
+        )
+        Updatable.__init__(self)
+        self.make_non_resizable()
+        self.center()
+        self.set_icon("res/test.gif")
+        self._counter = 0
+        self._canvas = tk.Canvas(self.get_tk_toplevel())
+        self._canvas.pack(expand=True, fill=tk.BOTH)  # fill parent
+        self._arc_id = self._canvas.create_arc(
+            2,
+            2,
+            width - 2,
+            height - 2,
+            start=0,
+            extent=120,
+            fill="#0000FF",
+            outline="#FFFFFF",
+        )
+        self._image = tk.PhotoImage(file="res/test.gif")
+        self._image_id = self._canvas.create_image(
+            128, 128, anchor=tk.NW, image=self._image
+        )
+        self._text_id = self._canvas.create_text(
+            146, 146, anchor=tk.CENTER, text=""
+        )
+
+    def destroy(self):
+        self._image = None
+        self._canvas.destroy()
+        self._canvas = None
+        gui.window.Window.destroy(self)
+
+    def on_update(self):
+        self._counter += 1
+        self._canvas.itemconfig(self._arc_id, start=self._counter)
+        self._canvas.itemconfig(self._text_id, text=str(self._counter))
+
+
+def main():
+    application = Application()
+    window = Test_Window(application, x=100, y=100)
+    application.get_frame_updater().register(window)
+    application.run()
+    application.quit()
+
+
+if __name__ == "__main__":
+    main()
