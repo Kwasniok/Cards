04db284d7380a6c1ce23e1329d0bfa4500f00afa
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-05 17:20:27 +0200

    added game.game_window

---
+++ game/game_window.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+from gui.window import Window
+from gui.update import Updatable
+
+
+class Game_Window(Window, Updatable):
+    def __init__(self, application, x, y):
+        width = 512
+        height = 512
+        Window.__init__(
+            self,
+            application=application,
+            title="Game Window",
+            width=width,
+            height=height,
+            x=x,
+            y=y,
+        )
+        Updatable.__init__(self)
+        self.make_non_resizable()
+        self.center()
+        self.set_icon("res/test.gif")
+        self._counter = 0
+        self._canvas = tk.Canvas(self.get_tk_toplevel())
+        self._canvas.pack(expand=True, fill=tk.BOTH)  # fill parent
+        self._image = tk.PhotoImage(file="res/test.gif")
+        self._image_id = self._canvas.create_image(
+            128, 128, anchor=tk.NW, image=self._image
+        )
+
+    def destroy(self):
+        self._image = None
+        self._image_id = None
+        self._canvas.destroy()
+        self._canvas = None
+        Window.destroy(self)
+
+    def on_update(self):
+        pass
