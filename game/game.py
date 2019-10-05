8252995aba05e94a826151997721dbffe38574c9
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-05 17:37:52 +0200

    fixed private attribute name in game.game

--- game/game.py
+++ game/game.py
@@ -8,7 +8,7 @@ class Game:
         self._name = name
         self._player1 = Player(name="player 1", color=BLACK)
         self._player2 = Player(name="player 2", color=WHITE)
-        self.game_state = Game_State("game state of " + str(self))
+        self._game_state = Game_State("game state of " + str(self))
 
     def __str__(self):
         return self._name
