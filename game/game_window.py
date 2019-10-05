33cb10dabf0a9fc308a77904b57d81fcf061205f
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-05 21:42:47 +0200

    removed internal counter in game.game_window

--- game/game_window.py
+++ game/game_window.py
@@ -21,7 +21,6 @@ class Game_Window(Window, Updatable):
         self.make_non_resizable()
         self.center()
         self.set_icon("res/game_icon.gif")
-        self._counter = 0
         self._canvas = tk.Canvas(self.get_tk_toplevel())
         self._canvas.pack(expand=True, fill=tk.BOTH)  # fill parent
         self._top_bar_rectangle_id = None
