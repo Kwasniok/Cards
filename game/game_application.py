dfd3f87a7f8d226ecdcf00ad2a6ef8f2a27f5879
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-05 17:33:18 +0200

    extended game.game_application  with game.game attribute

--- game/game_application.py
+++ game/game_application.py
@@ -1,10 +1,15 @@
 from gui.application import Application
 from .game_window import Game_Window
+from .game import Game
 
 
 class Game_Application(Application):
     def __init__(self):
         Application.__init__(self)
+        self._game = Game(name="main game")
+
+    def get_game(self):
+        return self._game
 
     def run(self):
         window = Game_Window(self, x=100, y=100)
