a0cb42faab0b1d3f6035788193e778f0620de78e
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-05 17:54:30 +0200

    removed test image in game.game_window

--- game/game_window.py
+++ game/game_window.py
@@ -23,14 +23,8 @@ class Game_Window(Window, Updatable):
         self._counter = 0
         self._canvas = tk.Canvas(self.get_tk_toplevel())
         self._canvas.pack(expand=True, fill=tk.BOTH)  # fill parent
-        self._image = tk.PhotoImage(file="res/test.gif")
-        self._image_id = self._canvas.create_image(
-            128, 128, anchor=tk.NW, image=self._image
-        )
 
     def destroy(self):
-        self._image = None
-        self._image_id = None
         self._canvas.destroy()
         self._canvas = None
         Window.destroy(self)
