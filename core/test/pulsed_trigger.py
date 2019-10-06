a5c5e43a84ef7d5065d38302133f0a93c5ea01c6
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-06 18:08:48 +0200

    splitted core.test.pulsed_trigger test cases

--- core/test/pulsed_trigger.py
+++ core/test/pulsed_trigger.py
@@ -18,12 +18,48 @@ class Listener:
 
 
 class Test(unittest.TestCase):
-    def test_general(self):
+    def test_start_stop(self):
+        pulsed_trigger = Pulsed_Trigger(max_pps=1)
+        listener = Listener()
+        # register listener
+        pulsed_trigger.register(listener, Listener.on_pull)
+        # test pull
+        listener.reset()
+        Pulsed_Trigger.pull_all()
+        self.assertEquals(listener.pull_count(), 1)
+        # stop
+        pulsed_trigger.stop()
+        listener.reset()
+        Pulsed_Trigger.pull_all()
+        self.assertEquals(listener.pull_count(), 0)
+        # restart
+        pulsed_trigger.restart()
+        listener.reset()
+        Pulsed_Trigger.pull_all()
+        self.assertEquals(listener.pull_count(), 1)
+
+    def test_registration(self):
         pulsed_trigger = Pulsed_Trigger(max_pps=5000)
         listener = Listener()
         # register listener
         pulsed_trigger.register(listener, Listener.on_pull)
         # test pull (forced pull)
+        listener.reset()
+        Pulsed_Trigger.pull_all()
+        self.assertEquals(listener.pull_count(), 1)
+        # unregister listener
+        listener.reset()
+        pulsed_trigger.unregister(listener)
+        Pulsed_Trigger.pull_all()
+        self.assertEquals(listener.pull_count(), 0)
+
+    def test_performance(self):
+        pulsed_trigger = Pulsed_Trigger(max_pps=5000)
+        listener = Listener()
+        # register listener
+        pulsed_trigger.register(listener, Listener.on_pull)
+        # test pull (forced pull)
+        listener.reset()
         Pulsed_Trigger.pull_all()
         self.assertEquals(listener.pull_count(), 1)
         # measure the pull frequency R times
@@ -43,14 +79,3 @@ class Test(unittest.TestCase):
             # measure actual pull frequency
             real_pps = float(N) / (t1 - t0)
             self.assertTrue(pulsed_trigger._max_pps > real_pps)
-        # unregister listener
-        pulsed_trigger.unregister(listener)
-        # n more pulls
-        n = 3
-        dummy_listener = Listener()
-        # register listener
-        pulsed_trigger.register(dummy_listener, Listener.on_pull)
-        while dummy_listener.pull_count() < n:
-            Pulsed_Trigger.update_all()
-        # listener must remain unchanged
-        self.assertEquals(N, listener._pulled)
