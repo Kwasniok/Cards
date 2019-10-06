ff8f8a83ac85926bfa48e6cf63cffcfa2e966d02
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-06 17:26:15 +0200

    fixed timing issue in core.pulsed_trigger

--- core/pulsed_trigger.py
+++ core/pulsed_trigger.py
@@ -10,13 +10,22 @@ from .trigger import Trigger
 class Pulsed_Trigger(Trigger):
     def __init__(self, max_pps):
         Trigger.__init__(self)
-        self._last_frame_time = time.time()
+        self._time_of_next_pull = None
         self._max_pps = max_pps
+        self.restart()
+
+    def restart(self):
+        # first pull is one period in the furutre or later
+        self._time_of_next_pull = time.perf_counter() + 1.0 / self._max_pps
 
     def update(self):
-        t = time.time()
-        if (t - self._last_frame_time) >= 1.0 / self._max_pps:
-            self._last_frame_time = t
+        t = time.perf_counter()
+        if t >= self._time_of_next_pull:
+            # next pull is the smalles integral amount of periods after the last which is in the future (pull drops on heavy load)
+            while t >= self._time_of_next_pull:
+                self._time_of_next_pull = (
+                    self._time_of_next_pull + 1.0 / self._max_pps
+                )
             self.pull()
             return True
         return False
