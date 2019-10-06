c0c7438d0b8370ab13fe5982bee68b48041f9c2a
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-06 22:32:21 +0200

    fixed missing destroy for application in main

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from game.game_application import Game_Application
 def main():
     application = Game_Application()
     application.run()
+    application.destroy()
 
 
 if __name__ == "__main__":
