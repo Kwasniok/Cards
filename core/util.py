4f8daeef7ddda90c3dea88ee44a3a1d03a1068c2
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-07 21:30:33 +0200

    added core.util.Randomized

--- core/util.py
+++ core/util.py
@@ -1,11 +1,26 @@
-from random import randrange
+import random
+import builtins
 
 
 def random_pick(list):
-    return list[randrange(len(list))]
+    return list[random.randrange(len(list))]
 
 
 def random_pop(list):
     elem = random_pick(list)
     list.remove(elem)
     return elem
+
+
+class Randomized:
+    def __init__(self, list):
+        self._list = list
+        self._order = builtins.list(range(len(self._list)))
+        random.shuffle(self._order)
+
+    def __len__(self):
+        return len(self._list)
+
+    def __iter__(self):
+        for x in self._order:
+            yield self._list[x]
