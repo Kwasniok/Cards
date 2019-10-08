8baedc17c0a25da3cefe10feac5828226af71f58
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-08 16:51:50 +0200

    unified naming convention of core.slot and core.two_sided_stack

--- core/two_sided_stack.py
+++ core/two_sided_stack.py
@@ -1,43 +1,43 @@
 class Two_Sided_Stack:
     def __init__(self):
-        self._data = []
+        self._objects = []
 
     def __str__(self):
-        return str(self._data)
+        return str(self._objects)
 
     def __repr__(self):
-        return repr(self._data)
+        return repr(self._objects)
 
     def push_left(self, elem):
-        self._data.insert(0, elem)
+        self._objects.insert(0, elem)
 
     def push_right(self, elem):
-        self._data.append(elem)
+        self._objects.append(elem)
 
     def pop_left(self):
-        elem = self._data[0]
-        del self._data[0]
+        elem = self._objects[0]
+        del self._objects[0]
         return elem
 
     def pop_right(self):
-        elem = self._data[-1]
-        del self._data[-1]
+        elem = self._objects[-1]
+        del self._objects[-1]
         return elem
 
     def get_left(self):
-        return self._data[0]
+        return self._objects[0]
 
     def get_right(self):
-        return self._data[-1]
+        return self._objects[-1]
 
     def __contains__(self, elem):
-        return elem in self._data
+        return elem in self._objects
 
     def __len__(self):
-        return len(self._data)
+        return len(self._objects)
 
     def __iter__(self):
-        return iter(self._data)
+        return iter(self._objects)
 
     def __getitem__(self, key):
-        return self._data[key]
+        return self._objects[key]
