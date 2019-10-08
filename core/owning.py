4d6b69479246ef986628b0645714a8336987b1ea
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-08 17:29:47 +0200

    changed core.owning interface

--- core/owning.py
+++ core/owning.py
@@ -1,29 +1,32 @@
+from abc import abstractmethod
+
+
 class Owner:
-    def __init__(self, name, color=None):
-        self._name = name
-        self._color = color
-
-    def __str__(self):
-        return self._name
-
-    def color(self, new=None):
-        if new is None:
-            old = self._color
-            self._color = new
-        else:
-            return self._color
-
-
-neutral_owner = Owner("neutral")
+    @abstractmethod
+    def __init__(self):
+        pass
 
 
 class Owned:
-    def __init__(self, owner=neutral_owner):
-        self._owner = owner
+    @abstractmethod
+    def __init__(self, owner):
+        self._owner = None
+        self.change_owner(owner)
 
-    def owner(self, new_owner=None):
-        if new_owner is None:
-            return self._owner
-        else:
-            new_owner, self._owner = self._owner, new_owner
-            return new_owner
+    def get_owner(self):
+        return self._owner
+
+    def change_owner(self, owner):
+        if not isinstance(owner, Owner):
+            raise (
+                TypeError(
+                    "Cannot change owner of "
+                    + str(self)
+                    + " due to invalid owner "
+                    + str(owner)
+                    + " (must be a "
+                    + str(Owner)
+                    + ")."
+                )
+            )
+        self._owner = owner
--- core/test/owning.py
+++ core/test/owning.py
@@ -3,17 +3,26 @@ from ..owning import Owner, Owned
 
 
 class Test(unittest.TestCase):
-    def test_owner(self):
-        o = Owner(name="test owner")
+    def test_abstract(self):
+        with self.assertRaises(TypeError):
+            Owned()
 
     def test_owned(self):
         class Test_Owned(Owned):
             def __init__(self, owner):
                 Owned.__init__(self, owner)
 
-        owner1 = Owner(name="test owner 1")
-        owner2 = Owner(name="test owner 2")
+        class Test_Owner(Owner):
+            def __init__(self):
+                Owner.__init__(self)
+
+        owner1 = Test_Owner()
+        owner2 = Test_Owner()
         owned = Test_Owned(owner1)
-        self.assertEqual(owned.owner(), owner1)
-        self.assertEqual(owned.owner(owner2), owner1)
-        self.assertEqual(owned.owner(), owner2)
+        with self.assertRaises(TypeError):
+            Test_Owned(1)
+        self.assertEqual(owned.get_owner(), owner1)
+        owned.change_owner(owner2)
+        self.assertEqual(owned.get_owner(), owner2)
+        with self.assertRaises(TypeError):
+            owned.change_owner(1)
