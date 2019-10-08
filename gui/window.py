f8efe11113893a505604bb339ffc7d2f49e9d658
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-08 21:26:06 +0200

    added hand display to gui.window

--- gui/window.py
+++ gui/window.py
@@ -32,10 +32,31 @@ class Window(Base_Window):
             command=lambda: self.on_update_realms(None),
         )
         self._update_realms_button.place(
-            anchor=tk.CENTER, x=width / 2, y=height / 2, width=200, height=100
+            anchor=tk.CENTER,
+            x=width / 2 - 100,
+            y=height / 2,
+            width=200,
+            height=100,
         )
         self._ream1_buttons = []
         self._ream2_buttons = []
+        self._update_hands_button = tk.Button(
+            self.get_tk_toplevel(),
+            text="update hands",
+            font=tkfont.Font(
+                family="Helvetica", size=px_to_pt(24), weight="bold"
+            ),
+            command=lambda: self.on_update_hands(None),
+        )
+        self._update_hands_button.place(
+            anchor=tk.CENTER,
+            x=width / 2 + 100,
+            y=height / 2,
+            width=200,
+            height=100,
+        )
+        self._hand1_buttons = []
+        self._hand2_buttons = []
 
     def destroy(self):
         if not (self._game_canvas is None):
@@ -43,6 +64,8 @@ class Window(Base_Window):
             self._game_canvas = None
         self._destroy_realm_buttons(self._ream1_buttons)
         self._destroy_realm_buttons(self._ream2_buttons)
+        self._destroy_hand_buttons(self._hand1_buttons)
+        self._destroy_hand_buttons(self._hand2_buttons)
         Base_Window.destroy(self)
 
     def _destroy_realm_buttons(self, realm_buttons):
@@ -50,6 +73,10 @@ class Window(Base_Window):
             for cell in column:
                 for button in cell:
                     button.destroy()
+
+    def _destroy_hand_buttons(self, hand_buttons):
+        for button in hand_buttons:
+            button.destroy()
 
     def on_update_realms(self, context):
         toplevel = self.get_tk_toplevel()
@@ -96,6 +123,56 @@ class Window(Base_Window):
             self._ream2_buttons,
             columns,
             rows,
+            x_0,
+            y_0,
+            button_width,
+            button_height,
+            button_padding,
+        )
+
+    def on_update_hands(self, context):
+        toplevel = self.get_tk_toplevel()
+        toplevel.update_idletasks()
+        width = toplevel.winfo_width()
+        height = toplevel.winfo_height()
+        game_state = self.get_application().get_game_state()
+        player1 = game_state.get_player1()
+        player2 = game_state.get_player2()
+        hand1 = player1.get_hand()
+        hand2 = player2.get_hand()
+
+        # clear hand GUI
+        self._destroy_hand_buttons(self._hand1_buttons)
+        self._destroy_hand_buttons(self._hand2_buttons)
+
+        # draw hand GUI
+        button_width = 180
+        button_height = 50
+        button_padding = 10
+        # hand 1
+        columns = hand1.get_size()
+        x_0 = (width - button_width * (columns - 1)) / 2
+        y_0 = int(button_height * 0.5)
+        self._create_hand_buttons(
+            context,
+            hand1,
+            self._hand1_buttons,
+            columns,
+            x_0,
+            y_0,
+            button_width,
+            button_height,
+            button_padding,
+        )
+        # hand 2
+        columns = hand2.get_size()
+        x_0 = (width - button_width * (columns - 1)) / 2
+        y_0 = height - int(button_height * 0.5)
+        self._create_hand_buttons(
+            context,
+            hand2,
+            self._hand2_buttons,
+            columns,
             x_0,
             y_0,
             button_width,
@@ -168,3 +245,32 @@ class Window(Base_Window):
                     shift += 10
                 button_column.append(button_cell)
             buttons.append(button_column)
+
+    def _create_hand_buttons(
+        self,
+        context,
+        hand,
+        buttons,
+        columns,
+        x_0,
+        y_0,
+        button_width,
+        button_height,
+        button_padding,
+    ):
+        for x in range(columns):
+            card = hand[x]
+            card_symbol = card.get_title(context)
+            button = tk.Button(
+                self.get_tk_toplevel(),
+                text=card_symbol,
+                command=lambda card=card: print("clicked on " + repr(card)),
+            )
+            button.place(
+                anchor=tk.CENTER,
+                x=x_0 + x * button_width,
+                y=y_0,
+                width=int(button_width - 0.5 * button_padding),
+                height=int(button_height - 0.5 * button_padding),
+            )
+            buttons.append(button)
