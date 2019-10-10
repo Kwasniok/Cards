eb629d398dd2b323b6cb006001bd2e1cd7fa70a8
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-10 23:45:47 +0200

    fixed card text issue with monastery and storage

--- game/assets/small_building_cards.py
+++ game/assets/small_building_cards.py
@@ -6,9 +6,6 @@ class Monastery_Card(Small_Building_Card):
     def __init__(self):
         Small_Building_Card.__init__(self, "Monastery")
 
-    def get_text(self, context):
-        return "<-protected form robber->"
-
     def get_cost(self, context):
         return [LOGS, IRON, BRICKS]
 
@@ -19,6 +16,9 @@ expansion_card_library.register(Monastery_Card, amount=2)
 class Storage_Card(Small_Building_Card):
     def __init__(self):
         Small_Building_Card.__init__(self, "Storage")
+
+    def get_text(self, context):
+        return "<-protected form robber->"
 
     def get_cost(self, context):
         return [LOGS, BRICKS]
