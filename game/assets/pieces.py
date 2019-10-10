cc6a01e4592ee8a0d972f16c1441dcf693da6f0f
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-10 23:04:57 +0200

    added knight piece

--- game/assets/pieces.py
+++ game/assets/pieces.py
@@ -11,3 +11,14 @@ class Mill_Piece(Piece):
 
 
 piece_library.register(Mill_Piece, amount=1)
+
+
+class Knight_Piece(Piece):
+    def __init__(self, owner=neutral_owner):
+        Piece.__init__(self, name="knight", owner=owner)
+
+    def get_win_points(self, context):
+        return 1
+
+
+piece_library.register(Knight_Piece, amount=1)
