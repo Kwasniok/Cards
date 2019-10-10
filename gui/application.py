244d860b419ecc3494f5a2a510d76e42d4dfd4d1
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-10 23:17:24 +0200

    removed dummy hand cards in gui.application (reverse of dde033d)

--- gui/application.py
+++ gui/application.py
@@ -13,14 +13,5 @@ class Application(Base_Application):
 
     def run(self):
         self._game_state.on_prepare_game()
-        # add dummy cards to hand to test gui
-        # TODO: remove!
-        from game.assets.small_building_cards import Monastery_Card
-
-        self._game_state.get_player1().get_hand().add(Monastery_Card())
-        self._game_state.get_player1().get_hand().add(Monastery_Card())
-        self._game_state.get_player2().get_hand().add(Monastery_Card())
-        self._game_state.get_player2().get_hand().add(Monastery_Card())
-
         window = Window(self, x=100, y=100)
         Base_Application.run(self)
