9b03cc0ef09cc981584c528c0381c5f9aa535ace
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-10 21:55:19 +0200

    added mill piece

--- game/assets/pieces.py
+++ game/assets/pieces.py
@@ -1,2 +1,10 @@
 from ..neutral_owner import neutral_owner
 from ..piece import Piece
+
+
+class Mill_Piece(Piece):
+    def __int__(self, owner=neutral_owner):
+        Piece.__int__(self, "mill", owner)
+
+    def get_mill_points(self, context):
+        return 1
