e01c3906b5740b3f233dbcd3eb9ecdaeebf33de7
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-10 23:46:53 +0200

    added resource doubler cards (primitive)

--- game/assets/small_building_cards.py
+++ game/assets/small_building_cards.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 from game.resource_types import *
 from game.expansion_cards import expansion_card_library, Small_Building_Card
 
@@ -28,3 +29,71 @@ class Storage_Card(Small_Building_Card):
 
 
 expansion_card_library.register(Storage_Card, amount=2)
+
+
+class Resource_Doubler_Card(Small_Building_Card):
+    # TODO: implement effect on incrementing neighbouring resource cards
+    @abstractmethod
+    def __init__(self, name, resource_type):
+        Small_Building_Card.__init__(self, name)
+        self._resource_type = resource_type
+
+    def get_text(self, context):
+        return (
+            "<- " + self._resource_type.get_source_name() + "s yield twice ->"
+        )
+
+
+class Sawmill_Card(Resource_Doubler_Card):
+    def __init__(self):
+        Resource_Doubler_Card.__init__(self, "Sawmill", LOGS)
+
+    def get_cost(self, context):
+        return [LOGS, LOGS]
+
+
+expansion_card_library.register(Sawmill_Card, amount=1)
+
+
+class Brickyard_Card(Resource_Doubler_Card):
+    def __init__(self):
+        Resource_Doubler_Card.__init__(self, "Brickyard", BRICKS)
+
+    def get_cost(self, context):
+        return [IRON, BRICKS]
+
+
+expansion_card_library.register(Brickyard_Card, amount=1)
+
+
+class Wool_Manufatory_Card(Resource_Doubler_Card):
+    def __init__(self):
+        Resource_Doubler_Card.__init__(self, "Wool Manufatory", WOOL)
+
+    def get_cost(self, context):
+        return [LOGS, BRICKS]
+
+
+expansion_card_library.register(Wool_Manufatory_Card, amount=1)
+
+
+class Ironforge_Card(Resource_Doubler_Card):
+    def __init__(self):
+        Resource_Doubler_Card.__init__(self, "Ironforge", IRON)
+
+    def get_cost(self, context):
+        return [IRON, BRICKS]
+
+
+expansion_card_library.register(Ironforge_Card, amount=1)
+
+
+class Mill_Card(Resource_Doubler_Card):
+    def __init__(self):
+        Resource_Doubler_Card.__init__(self, "Mill", GRAIN)
+
+    def get_cost(self, context):
+        return [BRICKS, GRAIN]
+
+
+expansion_card_library.register(Mill_Card, amount=1)
