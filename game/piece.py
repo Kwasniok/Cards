ccaec1e322dd69ea4396e95d145d7b217025c1ea
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-10 21:54:23 +0200

    extended game.piece interface

--- game/piece.py
+++ game/piece.py
@@ -1,9 +1,27 @@
+from abc import abstractmethod
 from core.internally_named import Internally_Named
 from core.owning import Owned
 from .neutral_owner import neutral_owner
+from .subtype_library import Subtype_Library
 
 
 class Piece(Internally_Named, Owned):
-    def __init__(self, name):
+    @abstractmethod
+    def __init__(self, name, owner=neutral_owner):
         Internally_Named.__init__(self, name=name)
-        Owned.__init__(self, owner=neutral_owner)
+        Owned.__init__(self, owner=owner)
+
+    def get_name(self, context):
+        return self._get_internal_name()
+
+    def get_mill_points(self, context):
+        return 0
+
+    def get_knight_points(self, context):
+        return 0
+
+    def get_win_points(self, context):
+        return 0
+
+
+piece_library = Subtype_Library(Piece)
