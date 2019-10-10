66aa44392d108b8ef8fbe67f5a6ec8b8c6118952
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-10 19:56:32 +0200

    added large habour card

--- game/assets/large_building_cards.py
+++ game/assets/large_building_cards.py
@@ -1,2 +1,16 @@
 from game.resource_types import *
 from game.expansion_cards import register_expansion_card, Large_Building_Card
+
+
+class Habour_Card(Large_Building_Card):
+    def __init__(self):
+        Large_Building_Card.__init__(self, "Habour")
+
+    def get_cost(self, context):
+        return [WOOL, IRON, BRICKS]
+
+    def get_mill_points(self, context):
+        return 1
+
+
+register_expansion_card(Habour_Card, 1)
