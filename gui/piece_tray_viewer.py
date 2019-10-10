9f6af8ca3b749b9eeffa837bc06d2bc58535594a
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-10 22:51:36 +0200

    added gui.piece_tray_viewer

---
+++ gui/piece_tray_viewer.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+import tkinter.font as tkfont
+from .piece_viewer import Piece_Viewer
+
+
+class Piece_Tray_Viewer:
+    @staticmethod
+    def create_buttons(context, window, piece_tray, x, y, width, height):
+        buttons = []
+        x_piece = x
+        y_piece = y
+        for piece in piece_tray:
+            button = Piece_Viewer.create_button(
+                context=context,
+                window=window,
+                piece=piece,
+                x=x_piece,
+                y=y_piece,
+                width=width,
+                height=height,
+            )
+            buttons.append(button)
+            x_piece += width
+        return buttons
