d62255971e5f3831a2763e026233768c445fae50
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-13 00:15:42 +0200

    extended core.test.internally_named

--- core/test/internally_named.py
+++ core/test/internally_named.py
@@ -9,5 +9,7 @@ class Test(unittest.TestCase):
 
         obj = Test_Internally_Named(name="test name")
         self.assertTrue(str(obj) == "test name")
+        self.assertTrue(obj._get_internal_name() == "test name")
         obj._set_internal_name("new test name")
         self.assertTrue(str(obj) == "new test name")
+        self.assertTrue(obj._get_internal_name() == "new test name")
