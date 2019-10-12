5068e207c9b8495789c8ee4a38fa5f3fca2fdada
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-13 01:42:31 +0200

    added game.test.game_state

--- game/test/game_state.py
+++ game/test/game_state.py
@@ -1,7 +1,27 @@
 import unittest
 from ..game_state import Game_State
+from ..player import Player
+from ..neutral_zone import Neutral_Zone
 
 
 class Test(unittest.TestCase):
     def test_general(self):
-        g = Game_State(name="test game state")
+        game_state = Game_State(name="test game state")
+        self.assertEqual(str(game_state), "test game state")
+        self.assertEqual(game_state.get_name(), "test game state")
+        # getters
+        player1 = game_state.get_player1()
+        self.assertTrue(isinstance(player1, Player))
+        player2 = game_state.get_player2()
+        self.assertTrue(isinstance(player2, Player))
+        neutral_zone = game_state.get_neutral_zone()
+        self.assertTrue(isinstance(neutral_zone, Neutral_Zone))
+        # on_prepare_game
+        game_state.on_prepare_game(context=None)
+        # getters
+        player1 = game_state.get_player1()
+        self.assertTrue(isinstance(player1, Player))
+        player2 = game_state.get_player2()
+        self.assertTrue(isinstance(player2, Player))
+        neutral_zone = game_state.get_neutral_zone()
+        self.assertTrue(isinstance(neutral_zone, Neutral_Zone))
