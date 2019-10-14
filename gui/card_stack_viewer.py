9193cfe427071fd4899cad84a8c2629d428cd56b
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-15 00:52:24 +0200

    added identity button for card slots

--- gui/card_stack_viewer.py
+++ gui/card_stack_viewer.py
@@ -21,8 +21,29 @@ class Card_Stack_Viewer:
         display_win_points=True,
     ):
         buttons = []
-        x_card = x
-        y_card = y
+        # card stack
+        handle_width = width
+        handle_height = int(height * 0.2)
+        # symbol
+        card_stack_symbol = card_stack.get_name()
+        card_stack_symbol += " (" + str(len(card_stack)) + ")"
+        # button
+        button = tk.Button(
+            window.get_tk_toplevel(),
+            text=card_stack_symbol,
+            command=lambda card_stack=card_stack: window.get_interaction_window().add_object(
+                card_stack
+            ),
+        )
+        button.place(
+            anchor=tk.CENTER,
+            x=x,
+            y=y - (height + handle_height) / 2,
+            width=handle_width,
+            height=handle_height,
+        )
+        buttons.append(button)
+
         for card in card_stack:
             button = Card_Viewer.create_button(
                 context=context,
