381ee4d6bbbbb8a7ebd98501a523c21c2e5389aa
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-15 01:10:02 +0200

    added idientity button in gui.hand_viewer

--- gui/hand_viewer.py
+++ gui/hand_viewer.py
@@ -47,6 +47,33 @@ class Hand_Viewer:
         y_0 = int(card_button_height * 0.5)
         if self._direction == directions.DOWN:
             y_0 = height - int(card_button_height * 0.5)
+
+        # hand
+        hand_card_button_width = columns * card_button_width
+        hand_card_button_height = int(card_button_height * 0.4)
+        hand_x = x_0 - (card_button_width - hand_card_button_width) / 2
+        if self._direction == directions.UP:
+            hand_y = y_0 + (card_button_height + hand_card_button_height) / 2
+        if self._direction == directions.DOWN:
+            hand_y = y_0 - (card_button_height + hand_card_button_height) / 2
+        hand_symbol = self._hand.get_name()
+        button = tk.Button(
+            self._window.get_tk_toplevel(),
+            text=hand_symbol,
+            command=lambda hand=self._hand: self._window.get_interaction_window().add_object(
+                hand
+            ),
+        )
+        button.place(
+            anchor=tk.CENTER,
+            x=hand_x,
+            y=hand_y,
+            width=hand_card_button_width,
+            height=hand_card_button_height,
+        )
+        self._buttons.append(button)
+
+        # cards in hand
         for x in range(columns):
             card = self._hand[x]
             self._buttons.append(
