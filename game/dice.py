65d761d317f4ca67bd2429fbff2bdc0dbb5ed2a5
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-15 00:05:54 +0200

    game.dice is now owned

--- game/dice.py
+++ game/dice.py
@@ -1,10 +1,13 @@
 from core.internally_named import Internally_Named
+from core.owning import Owned
 from core.dice import Dice as Base_Dice
+from .neutral_owner import neutral_owner
 
 
-class Dice(Internally_Named, Base_Dice):
+class Dice(Internally_Named, Owned, Base_Dice):
     def __init__(self, name, outcomes):
         Internally_Named.__init__(self, name)
+        Owned.__init__(self, neutral_owner)
         Base_Dice.__init__(self, outcomes)
 
     def get_name(self):
