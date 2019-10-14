99b7243e14660b3a56a486efe6f0cea19f02b534
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-15 01:30:34 +0200

    added identity button in gui.realm_viewer

--- gui/realm_viewer.py
+++ gui/realm_viewer.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import tkinter.font as tkfont
+from core.destroy import safe_destroy
 import game.directions as directions
 from .card_viewer import Card_Viewer
 from .slot_viewer import Slot_Viewer
@@ -21,12 +22,14 @@ class Realm_Viewer:
         self._window = window
         self._realm = realm
         self._direction = direction
+        self._realm_button = None
         self._buttons = []
 
     def destroy(self):
         self._destroy_buttons()
 
     def _destroy_buttons(self):
+        self._realm_button = safe_destroy(self._realm_button)
         for column in self._buttons:
             for cell in column:
                 for button in cell:
@@ -43,14 +46,43 @@ class Realm_Viewer:
         self._destroy_buttons()
 
         # create buttons
-        slot_button_width = 180
-        slot_button_height = 80
         columns = len(self._realm.get_card_slot_grid())
         rows = 5
-        x_0 = (width - slot_button_width * (columns - 1)) / 2
+        slot_button_width = 180
+        slot_button_height = 80
+        realm_button_width = slot_button_width * max(columns, 1)
+        realm_button_height = 20
+        x_0 = (width - slot_button_width * max(0, columns - 1)) / 2
         y_0 = 70 + int(slot_button_height * 0.5)
         if self._direction == directions.DOWN:
             y_0 = height - int(slot_button_height * ((rows - 1))) - y_0
+        realm_x = x_0 + (slot_button_width * (columns - 1)) / 2
+        if self._direction == directions.UP:
+            realm_y = (
+                y_0
+                + slot_button_height * (rows - 1)
+                + (slot_button_height + realm_button_height) / 2
+            )
+        if self._direction == directions.DOWN:
+            realm_y = y_0 - (slot_button_height + realm_button_height) / 2
+        # realm
+        realm_symbol = self._realm.get_name()
+        button = tk.Button(
+            self._window.get_tk_toplevel(),
+            text=realm_symbol,
+            command=lambda realm=self._realm: self._window.get_interaction_window().add_object(
+                realm
+            ),
+        )
+        button.place(
+            anchor=tk.CENTER,
+            x=realm_x,
+            y=realm_y,
+            width=realm_button_width,
+            height=realm_button_height,
+        )
+        self._realm_button = button
+        # slots
         for x in range(columns):
             button_column = []
             for y in range(rows):
