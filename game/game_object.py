3ef19969367af90b25e07942122a461e2c7d8f5f
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-15 19:37:04 +0200

    added game.game_object interface

---
+++ game/game_object.py
@@ -0,0 +1,34 @@
+from abc import ABC, abstractmethod
+from core.internally_named import Internally_Named
+from core.owning import Owned
+from .neutral_owner import neutral_owner
+
+
+class Game_Object(ABC, Internally_Named, Owned):
+
+    _id_counter = 0
+
+    @staticmethod
+    def _next_id():
+        Game_Object._id_counter += 1
+        return Game_Object._id_counter
+
+    @abstractmethod
+    def __init__(self, name, owner=neutral_owner):
+        Internally_Named.__init__(self, name)
+        Owned.__init__(self, owner)
+        self._id = Game_Object._next_id()
+
+    def get_id(self):
+        return self._id
+
+    def __repr__(self):
+        return (
+            "Game_Object(name="
+            + str(self)
+            + ",owner="
+            + str(self.get_owner())
+            + ",id="
+            + str(self._id)
+            + ")"
+        )
