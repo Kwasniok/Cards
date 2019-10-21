51cc50828efc279d8b2a562e56db3322f26a8329
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-21 23:31:04 +0200

    updated game.subtype_library to support initializers

--- game/subtype_library.py
+++ game/subtype_library.py
@@ -1,25 +1,32 @@
 class Subtype_Library:
     def __init__(self, base_class):
         self._base_class = base_class
-        self._histogram = {}
+        self._register = []
 
-    def register(self, card_class, amount=1):
-        if not issubclass(card_class, self._base_class):
+    def register(self, clss, amount=1, initializer_dict=None):
+        if not issubclass(clss, self._base_class):
             raise (
                 ValueError(
                     "Cannot register card class "
-                    + str(card_class)
+                    + str(clss)
                     + " because it is not derived from the card base class "
                     + str(cars_base_class)
                     + "."
                 )
             )
-        # print("event card registered: " + card_class.__name__)
-        self._histogram[card_class] = amount
+        # print(
+        #     "registered: "
+        #     + clss.__qualname__
+        #     + " with initializer_dict="
+        #     + str(initializer_dict)
+        # )
+        self._register.append((clss, amount, initializer_dict))
 
     def get_all(self):
         objects = []
-        for clss, count in self._histogram.items():
-            for i in range(count):
-                objects.append(clss())
+        for (clss, amount, initializer_dict) in self._register:
+            if initializer_dict is None:
+                initializer_dict = {}
+            for i in range(amount):
+                objects.append(clss(**initializer_dict))
         return objects
