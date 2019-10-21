33bfeba984881e065f39291eda37e411eb4c73b1
Kwasniok <Kwasniok@users.noreply.github.com>
2019-10-22 01:13:57 +0200

    locked dice roll to dice roll phase only

--- game/dice.py
+++ game/dice.py
@@ -1,6 +1,6 @@
 from core.dice import Dice as Base_Dice
 from .game_object import Game_Object
-from .action import action
+from .action import action, Action_Invokation_Error
 
 
 class Dice(Game_Object, Base_Dice):
@@ -13,4 +13,10 @@ class Dice(Game_Object, Base_Dice):
 
     @action
     def on_roll(self, context):
+        if not "dice roll phase" in context.active_phases:
+            raise Action_Invokation_Error(
+                "Cannot roll dice "
+                + str(self)
+                + ": active phase is not roll dice phase."
+            )
         self.roll()
