5d29f42a51abbc127450e6a433076c919f807a99
Kwasniok <Kwasniok@users.noreply.github.com>
2019-11-12 00:19:35 +0100

    added abstract event card to assets which checks the phase on discovery

--- game/assets/event_cards.py
+++ game/assets/event_cards.py
@@ -1,5 +1,22 @@
+from abc import abstractmethod
 from game.action import action
-from game.event_card import event_card_library, Event_Card
+from game.event_card import event_card_library, Event_Card as Basic_Event_Card
+
+
+class Event_Card(Basic_Event_Card):
+    @abstractmethod
+    def __init__(self, name):
+        Basic_Event_Card.__init__(self, name)
+
+    def on_discovery_is_invokable(self, context):
+        return (
+            Basic_Event_Card.on_discovery_is_invokable(self, context)
+            and "event phase" in context.active_phases
+        )
+
+    @action(on_discovery_is_invokable)
+    def on_discovery(self, context):
+        Basic_Event_Card.on_discovery(self, context)
 
 
 class Turn_Of_The_Year_Card(Event_Card):
@@ -9,7 +26,7 @@ class Turn_Of_The_Year_Card(Event_Card):
     def get_text(self, context):
         return "The event card stack is shuffled."
 
-    @action()
+    @action(Event_Card.on_discovery_is_invokable)
     def on_discovery(self, context):
         print("TODO: shuffle event card stack")
         # TODO: shuffle event card stack
